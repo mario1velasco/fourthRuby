4b2d786cf807ce08ef2b9b899868c3f63ca41e57
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-23 09:36:32 +0100

    Feat: Show created element

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,27 @@
 class ArticlesController < ApplicationController
   def new
+    @article=Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    # @article.save
+    # redirect_to @article
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id]);
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
