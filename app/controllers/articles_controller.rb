ebec7f32f1977e384c5ef96be1d1b018285ae24e
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-23 10:28:45 +0100

    Feat: Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
-
-    # @article.save
-    # redirect_to @article
+    @article = Article.new(article_params)    
 
     if @article.save
       redirect_to @article
@@ -23,6 +20,19 @@ class ArticlesController < ApplicationController
   def index
     @articles= Article.all
   end
+
+  def edit
+    @article=Article.find(params[:id])
+  end
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
